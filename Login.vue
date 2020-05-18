a8f3482d6155bb3d32cbcedbc6484395fcc78761
FXW199809 <917839169@qq.com>
2020-05-18 10:43:07 +0800

    feat(第一次整合后端：登陆、修改密码、账号管理、审批、查看工资、下载模板、上传文件、导出工资表功能)doc(需求文档、原型设计、接口文档、数据库设计文档）

--- Login.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div>
-    <h1>欢迎登录财务管理系统</h1>
-    <hr>
-    <router-link to="/HelloWorld"> 财务管理员点击登录</router-link>
-    <router-link to="/Yuanximain"> 院系管理员点击登录</router-link>
-    <router-link to="/Normalmain"> 普通员工点击登录</router-link>
-</div>
-</template>
